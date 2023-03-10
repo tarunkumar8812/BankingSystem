dc2caac22b6c73d7de74281a1ec70562f040a3c1
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-10 15:16:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      start creating your Banking System application now...
+    </div>
+  );
+}
+
+export default App;
